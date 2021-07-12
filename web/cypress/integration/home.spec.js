7bdb5da440291289f7f7fedbed25db5cb9e6ed0c
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-12 20:57:59 +0530

    add another test case

--- web/cypress/integration/home.spec.js
+++ web/cypress/integration/home.spec.js
@@ -9,4 +9,8 @@ context("Home Page", () => {
     it("should find our welcome page and message", () => {
         cy.get("h1").contains("Welcome");
     })
+
+    it("should find our get started message", () => {
+        cy.get("p").contains("Get started by editing");
+    })
 })
