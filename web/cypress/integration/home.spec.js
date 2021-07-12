ca533c6986d665409bc47f6b5c76a326147761ce
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-12 20:54:16 +0530

    Update home.spec.js

--- web/cypress/integration/home.spec.js
+++ web/cypress/integration/home.spec.js
@@ -4,4 +4,9 @@ context("Home Page", () => {
     beforeEach(() => {
         cy.visit("http://localhost:3000")
     })
+
+
+    it("should find our welcome page and message", () => {
+        cy.get("h1").contains("Welcome");
+    })
 })
