9d76925a1553fcac320d68ce4d347c9ce2cb29a2
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-12 21:16:26 +0530

    added design card

--- web/pages/index.js
+++ web/pages/index.js
@@ -49,6 +49,15 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Design &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
         </div>
       </main>
 
